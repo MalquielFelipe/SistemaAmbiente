2100925c13f370d704a3db985cc7777268ccda7c
carloswilsonsjp <carloswilsonsjp@gmail.com>
2025-05-14 14:57:23 -0300

    14-05 15:57

--- cadastroAmbiente.php
+++ cadastroAmbiente.php
@@ -5,7 +5,7 @@ codigo CadastroAmbiente.php
 //verificar se os campos não nulos
     if(isset($_POST['nome']) && isset($_POST['capacidade']) &&
         isset($_POST['Bloco']) && isset($_POST['Ambiente']) &&
-        isset($_POST['nome'])&& inseet(tipo['tipo']) ){
+        isset($_POST['nome'])&& isset($_POST['tipo']) ){
             $capacidade = $_POST['capacidade'];
             $Bloco = $_POST['Bloco'];
             $ambiente = $_POST['Ambiente'];
